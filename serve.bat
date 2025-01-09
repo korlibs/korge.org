9b54ed218ffbfa82c069840e3c7cd3065c97e835
soywiz <soywiz@gmail.com>
2025-01-09 16:57:31 +0100

    More work

--- serve.bat
+++ serve.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
-cd .11ty & deno run -A npm:@11ty/eleventy --serve & set SAVEDRC=!ERRORLEVEL! & call;
+cd .11ty & deno install & deno run -A npm:@11ty/eleventy --serve & set SAVEDRC=!ERRORLEVEL! & call;
 EXIT /B %SAVEDRC%
